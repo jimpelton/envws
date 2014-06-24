b67fca3cc8081523f4086428fbaf87dfe475676d
Jim Pelton <jimpelton@boisestate.edu>
2014-06-24 07:31:44 -0600

    fixed wrong value being assigned in repover.ps1.

--- Envws/Resources/tools/repover.ps1
+++ Envws/Resources/tools/repover.ps1
@@ -12,4 +12,4 @@ Set-Location -Path $workingDir
 [string]$a = svn info . | Where-Object {([string]$_).StartsWith("Revision:") -eq $True} | %{ echo ([string]$_).Split(":")[1].Trim() }
 [Int32]$rev = 0
 [Int32]::TryParse($a, [ref]$rev)
-echo "namespace $namespace { public class $class { public static int VER=$a; } }" | Tee-Object -FilePath "$outpath\RepoVer.cs"
+echo "namespace $namespace { public class $class { public static int VER=$rev; } }" | Tee-Object -FilePath "$outpath\RepoVer.cs"
