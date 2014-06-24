ede0dae21f21f48fe184143bc25868a052e2bde8
Jim Pelton <jimpelton@boisestate.edu>
2014-06-24 11:52:00 -0600

    added ConsoleClient project

---
+++ ConsoleClient/ConsoleClient/ConsoleClient.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleClient
+{
+	using ClientLib.Client;
+    using EnvwsLib.DataContracts;
+
+    class ConsoleClient
+    {
+
+        private bool isInit = false;
+        private OrchestratorConnection oc;
+        private string orchestratorIpAddr;
+        private ManualResetEvent manualResetEvent;
+        private List<TrackerData> datas;
+
+        public ConsoleClient()
+        {
+            datas = new List<TrackerData>();
+            manualResetEvent = new ManualResetEvent(false);
+        }
+
+        public bool Init()
+        {
+            if (!isInit)
+            {
+                oc = new OrchestratorConnection();
+                oc.TrackersPinged += OnPingEvent;
+                oc.StartPinging();
+                isInit = true;
+            }
+
+            return isInit;
+        }
+
+        public void OnPingEvent(object sender, TrackersPingedEventArgs e)
+        {
+            datas.Clear();
+            datas.AddRange(e.Trackers);
+            manualResetEvent.Set();
+        }
+
+        public TrackerData[] GetTrackerData()
+        {
+            manualResetEvent.WaitOne();
+            return datas.ToArray();
+        }
+
+        public void SubmitJob(string envxFileName, string sourceUri, string resultsUri, string friendlyName, int[] scenarios)
+        {
+			try
+			{
+                oc.SubmitJob(envxFileName, sourceUri, resultsUri, friendlyName, scenarios);
+			}
+			catch (System.Exception ex)
+			{
+                Console.WriteLine("Failed to submit job: " + ex.Message);					
+			}
+        }
+    }
+}
---
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleClient
+{
+    using ClientLib;
+    using EnvwsLib.DataContracts; 
+
+    public class Command
+    {
+        public string Name { get; set; }
+        public string Args { get; set; }
+        public string Cmd { get; set; }
+    }
+
+    public class Program
+    {
+
+        private static Dictionary<string, Command> Commands =
+            new Dictionary<string, Command>() 
+            {
+                {"ls", 
+                    new Command { Name="list-trackers", Args="No arguments.", Cmd="ls"} },
+                {"s", 
+                    new Command { Name="submit-job", Args="<job name> <envx name> <source uri> <results uri> <scenarios[]>", Cmd="s"} }
+            };
+
+		private static readonly List<string> ShortCommands = 
+            new List<string>() 
+			{
+				"ls", "s"
+            };
+
+        public static void Main(string[] args)
+        {
+			// check if command is -v (print version information)
+			if (args.Length == 1)
+            {
+				if (args[0].ToLower() == "-v")
+				{
+					Console.WriteLine("Version: " + RepoVer.VER);
+					Environment.Exit(0);
+				}
+            }
+
+			// args[0]=<orch-endpoint> args[1]=<command> args[2..n]=optional args
+            if (args.Length < 2) 
+            { 
+                printUsageAndExit(); 
+            }
+            
+            string orchEndpoint = args[0];
+            string command = args[1];
+
+			// commands start with a leading '-'
+            if (!command.StartsWith("-")) 
+            { 
+                printUsageAndExit(); 
+            }
+
+			command = command.Substring(1);
+
+			// check that command is actually a command
+			if (!Commands.ContainsKey(command)) 
+            { 
+                printUsageAndExit(); 
+            }
+
+			string[] commandArgs;
+
+			// copy optional command arguments
+            if (args.Length > 2)
+            {   
+                // copy everything after the command
+                commandArgs = new string[args.Length - 2];
+                Array.Copy(args, 2, commandArgs, 0, commandArgs.Length);
+            }
+            else 
+            {  
+                commandArgs = new string[0]; 
+            }
+
+			// print command specific help
+            if (commandArgs.Length >= 1 && commandArgs[0].StartsWith("h")) 
+            { 
+                printUsageAndExit(command); 
+            }
+
+            ConsoleClient cc = new ConsoleClient();
+
+            if (command == "ls")
+                DoTrackerDatas(cc);
+            else if (command == "s")
+                DoSubmitJob(cc, commandArgs);
+        }
+
+        private static void DoTrackerDatas(ConsoleClient cc)
+        {
+            cc.Init();
+            List<TrackerData> data = new List<TrackerData>(cc.GetTrackerData());
+            if (data.Count == 0)
+            {
+                Console.WriteLine("No Trackers");
+            }
+            else
+            {
+                int running = data.Count(td => td.Status == TrackerStatus.RUNNING);
+                int down = data.Count(td => td.Status == TrackerStatus.NO_RESPONSE || td.Status == TrackerStatus.UNKNOWN);
+                int idle = data.Count(td => td.Status == TrackerStatus.IDLE);
+
+                Console.WriteLine(data.Count + " registered trackers.");
+				Console.WriteLine(running + " running.");
+				Console.WriteLine(down + " down.");
+				Console.WriteLine(idle+ " idle.");
+
+                foreach (TrackerData td in data)
+                {
+                    Console.WriteLine(td.Uri + " " + td.Status);
+                }
+            }
+        }
+
+        private static void DoSubmitJob(ConsoleClient cc, string[] args)
+        {
+            if (args.Length < 5)
+            {
+                usage(Commands["s"].Cmd);
+            }
+            else
+            {
+                string friendlyName = args[1];
+                string envxName = args[2];
+                string sourceUri = args[3];
+                string resultsUri = args[4];
+                int[] scenarios;
+
+                if (args[5] == null)
+                {
+                    scenarios = new int[] { 0 };
+                }
+                else
+                {
+                    string scenariosString = args[5];
+                    scenarios = scenariosString.Split(',')
+                        .Select(numStr => 
+							{ 
+								int a; 
+								Int32.TryParse(numStr, out a); 
+								return a; 
+							}
+                        ).ToArray();
+                }
+                cc.Init();
+                cc.SubmitJob(envxName, sourceUri, resultsUri, friendlyName, scenarios);
+            }
+        }
+
+        private static void printUsageAndExit()
+        {
+            usage("");
+            Environment.Exit(0);
+        }
+        
+        private static void printUsageAndExit(String cmd)
+        {
+            usage(cmd);
+            Environment.Exit(0);
+        }
+
+        private static void usage(String cmd)
+        {
+            string msg = 
+                "Usage: \n" +
+				"\t<endpoint> <command> [command-options]\n\n";
+
+            if (cmd == string.Empty)
+            {
+                msg += validCommandsUsageString();
+            }
+            else if (Commands.ContainsKey(cmd))
+            {
+                msg = "Usage for " + Commands[cmd].Name + ": -" + Commands[cmd].Cmd + " " + Commands[cmd].Args;
+            }
+            else
+            {
+                msg += "Unknown command.\n" + validCommandsUsageString();
+            }
+
+            Console.WriteLine(msg);
+        }
+
+        private static void usage()
+        {
+            usage("");
+        }
+
+        private static string validCommandsUsageString()
+        {
+            string msg = "\tValid Commands: \n";
+            foreach (Command c in Commands.Values)
+            {
+                msg += "\t" + c.Name + ":\t-" + c.Cmd + " " + c.Args + "\n";
+            }
+            return msg;
+        }
+    }
+}
