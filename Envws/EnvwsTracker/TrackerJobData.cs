5bd920ee64b34206f1163ce1e80c4cf30f7e4bdd
Jim Pelton <jimpelton@boisestate.edu>
2014-06-27 14:48:20 -0600

    added missing TrackerJobData.cs

---
+++ Envws/EnvwsTracker/TrackerJobData.cs
@@ -0,0 +1,24 @@
+﻿using EnvwsLib.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvwsTracker 
+{
+	/// <summary>
+	/// Associates a completed JobData with a boolean that indicates if the JobData
+    /// was successfully returned by the Tracker that executed it.
+	/// </summary>
+    public class TrackerJobData
+    {
+        public bool HasBeenReturnedToTracker { get; set; }
+        public JobData TheJobInQuestion { get; set; }
+
+		public TrackerJobData()
+        {
+            HasBeenReturnedToTracker = false;
+        }
+    }
+}
